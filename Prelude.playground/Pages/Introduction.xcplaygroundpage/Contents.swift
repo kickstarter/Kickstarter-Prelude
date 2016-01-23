3bf53d64c684dc997f8ecf20ef75fe5dbce404bf
Brandon Williams <mbw234@gmail.com>
2016-01-23 13:38:29 -0500

    Added getting started section to playground introduction.

--- Prelude.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
+++ Prelude.playground/Pages/Introduction.xcplaygroundpage/Contents.swift
@@ -7,6 +7,11 @@ A collection of tools that are used in the Kickstarter apps.
 
 ---
 
+## Getting started
+
+To execute these playground files you must first open `Prelude.xcworkspace` instead of the playground file directly,
+and hit `cmd+B` in order to build the framework.
+
 ## Table of contents
 
 * [Array.swift](Array.swift)
