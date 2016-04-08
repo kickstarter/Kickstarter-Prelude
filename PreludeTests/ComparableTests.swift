a06b85f989bb258415347a873ec71baec01e3b30
Brandon Williams <mbw234@gmail.com>
2016-04-08 08:24:02 -0400

    Fix clamp test.

--- PreludeTests/ComparableTests.swift
+++ PreludeTests/ComparableTests.swift
@@ -2,13 +2,6 @@ import XCTest
 @testable import Prelude
 
 class ComparableTests: XCTestCase {
-
-  func testClamp() {
-    XCTAssertEqualWithAccuracy(0.0, 0.0.clamp(1.0)(-1.0), accuracy: 0.0)
-    XCTAssertEqualWithAccuracy(0.5, 0.0.clamp(1.0)(0.5), accuracy: 0.0)
-    XCTAssertEqualWithAccuracy(1.0, 0.0.clamp(1.0)(2.0), accuracy: 0.0)
-  }
-
   func testClampFreeFunction() {
     XCTAssertEqualWithAccuracy(0.0, clamp(0.0, 1.0)(-1.0), accuracy: 0.0)
     XCTAssertEqualWithAccuracy(0.5, clamp(0.0, 1.0)(0.5), accuracy: 0.0)
