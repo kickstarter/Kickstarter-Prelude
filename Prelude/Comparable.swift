69f114ab72a4143cd7a758979da4b55e34b9aff6
Brandon Williams <mbw234@gmail.com>
2016-04-08 08:15:33 -0400

    Go rid of method version of clamp.

--- Prelude/Comparable.swift
+++ Prelude/Comparable.swift
@@ -1,19 +1,5 @@
-extension Comparable {
-  /**
-   Clamps a value between `self` and `max`.
-
-   - parameter max: The max value to clamp to.
-
-   - returns: A function that takes a value and returns the value clamped to [self, max].
-   */
-  public func clamp(max: Self) -> (Self -> Self) {
-    assert(self < max)
-    return { value in value < self ? self : value > max ? max : value }
-  }
-}
-
 /**
- Clamps a value between a min and max. This is a free-function version of `Comparable.clamp`.
+ Clamps a value between a min and max.
 
  - parameter min: The minimum value to clamp to.
  - parameter max: The maximum value to clamp to.
@@ -21,5 +7,6 @@ extension Comparable {
  - returns: A function that takes a value and returns the value clamped to [min, max].
  */
 public func clamp <T: Comparable> (min: T, _ max: T) -> (T -> T) {
-  return { value in value |> min.clamp(max) }
+  assert(min <= max)
+  return { value in value < min ? min : value > max ? max : value }
 }
