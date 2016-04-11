33a0d548ca8705381981793a0b1f4e105c78e939
Brandon Williams <mbw234@gmail.com>
2016-04-11 08:25:27 -0400

    Simplify array compact.
    There's an overloaded version of `flatMap` on arrays of optionals that does the work for us.

--- Prelude/Array.swift
+++ Prelude/Array.swift
@@ -4,7 +4,7 @@ public extension Array where Element: OptionalType {
    - returns: A new array with `nil` values removed.
    */
   public func compact() -> [Element.Wrapped] {
-    return self.filter { $0.optional != nil }.map { $0.optional! }
+    return self.flatMap(id)
   }
 }
 
