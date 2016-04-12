56b9fc2500604bbbbacab6d02b8790998368bac5
Brandon Williams <mbw234@gmail.com>
2016-04-12 10:25:54 -0400

    wip

---
+++ Prelude.playground/Pages/03-Either.swift.xcplaygroundpage/Contents.swift
@@ -0,0 +1,14 @@
+import Prelude
+
+/*:
+ # Either.swift
+ 
+ ---
+ 
+ The `Either` type represents values with two possibilities. In particular, if `x: Either<A, B>`,
+ then `x` either holds something of type `A` or something of type `B`, but never both at the 
+ same time.
+ 
+ A close cousin to `Either<A, B>` is the tuple `(A, B)`, and Swift provides first class support
+ for that type.
+ */
