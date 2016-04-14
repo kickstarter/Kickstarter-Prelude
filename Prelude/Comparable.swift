6a4da51bf0319a542039e03369baee6c5e30aaa4
Brandon Williams <mbw234@gmail.com>
2016-04-14 07:53:45 -0400

    CLAMPS

--- Prelude/Comparable.swift
+++ Prelude/Comparable.swift
@@ -10,3 +10,43 @@ public func clamp <T: Comparable> (min: T, _ max: T) -> (T -> T) {
   assert(min <= max)
   return { value in value < min ? min : value > max ? max : value }
 }
+
+public func clamp <T: Comparable> (min min: T) -> (T -> T) {
+  return { value in value < min ? min : value }
+}
+
+public func clamp <T: Comparable> (max max: T) -> (T -> T) {
+  return { value in value > max ? max : value }
+}
+
+/**
+ Sample usage:
+
+ ```
+ max(0.0)(-1.0) == 0.0
+ 5.0 |> max(2.0) == 5.0
+ ```
+
+ - parameter a: The value to take the `max` with.
+
+ - returns: A function that will take the `max` of `a` and its argument.
+ */
+public func max <T: Comparable> (a: T) -> (T -> T) {
+  return { max(a, $0) }
+}
+
+/**
+ Sample usage:
+
+ ```
+ min(0.0)(1.0) == 0.0
+ 2.0 |> min(5.0) == 2.0
+ ```
+
+ - parameter a: The value to take the `min` with.
+
+ - returns: A function that will take the `min` of `a` and its argument.
+ */
+public func min <T: Comparable> (a: T) -> (T -> T) {
+  return { max(a, $0) }
+}
