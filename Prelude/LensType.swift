a478eea879d7ac5b287911f64acb1a8faa416e81
Brandon Williams <mbw234@gmail.com>
2016-05-20 13:07:08 -0400

    Added a bunch of docs.

--- Prelude/LensType.swift
+++ Prelude/LensType.swift
@@ -17,9 +17,9 @@ public extension LensType {
    - returns: A function that takes wholes to wholes by applying the function to a subpart.
    */
   public func over(f: Part -> Part) -> (Whole -> Whole) {
-    return { (a: Whole) -> Whole in
-      let part = self.view(a)
-      return self.set(f(part), a)
+    return { whole in
+      let part = self.view(whole)
+      return self.set(f(part), whole)
     }
   }
 
@@ -34,27 +34,60 @@ public extension LensType {
   public func compose <RLens: LensType where RLens.Whole == Part>(rhs: RLens) -> Lens<Whole, RLens.Part> {
     return Lens(
       view: rhs.view • self.view,
-      set: { (c: RLens.Part, a: Whole) -> Whole in
-        let part = self.view(a)
-        let newPart = rhs.set(c, part)
-        return self.set(newPart, a)
+      set: { subPart, whole in
+        let part = self.view(whole)
+        let newPart = rhs.set(subPart, part)
+        return self.set(newPart, whole)
       }
     )
   }
 }
 
-public func *~ <L: LensType> (lens: L, b: L.Part) -> L.Whole -> L.Whole {
-  return { a in lens.set(b, a) }
+/**
+ Infix operator of the `set` function.
+
+ - parameter lens: A lens.
+ - parameter part: A part.
+
+ - returns: A function that transforms a whole into a new whole with a part replaced.
+ */
+public func *~ <L: LensType> (lens: L, part: L.Part) -> (L.Whole -> L.Whole) {
+  return { whole in lens.set(part, whole) }
 }
 
-public func ^* <L: LensType> (a: L.Whole, lens: L) -> L.Part {
-  return lens.view(a)
+/**
+ Infix operator of the `view` function.
+
+ - parameter whole: A whole.
+ - parameter lens:  A lens.
+
+ - returns: A part of a whole when viewed through the lens provided.
+ */
+public func ^* <L: LensType> (whole: L.Whole, lens: L) -> L.Part {
+  return lens.view(whole)
 }
 
+/**
+ Infix operator of `compose`, which composes two lenses.
+
+ - parameter lhs: A lens.
+ - parameter rhs: A lens.
+
+ - returns: The composed lens.
+ */
 public func • <A, B, C> (lhs: Lens<A, B>, rhs: Lens<B, C>) -> Lens<A, C> {
   return lhs.compose(rhs)
 }
 
+/**
+ Infix operator of Kleisli composition. This type of composition can help avoid need the full power of
+ prisms and traversals when all you need to do is compose lenses through an optional value.
+
+ - parameter lhs: A lens whose part is optional.
+ - parameter rhs: A lens whose part is optional.
+
+ - returns: The composed lens.
+ */
 public func >•> <A, B, C> (lhs: Lens<A, B?>, rhs: Lens<B, C?>) -> Lens<A, C?> {
   return Lens(
     view: { a in lhs.view(a).flatMap(rhs.view) },
@@ -62,10 +95,26 @@ public func >•> <A, B, C> (lhs: Lens<A, B?>, rhs: Lens<B, C?>) -> Lens<A, C?> 
   )
 }
 
-public func %~ <L: LensType> (lens: L, f: L.Part -> L.Part) -> L.Whole -> L.Whole {
+/**
+ Infix operator of the `over` function.
+
+ - parameter lens: A lens.
+ - parameter f:    A function for transforming a part of a whole.
+
+ - returns: A function that transforms a whole into a new whole with its part transformed by `f`.
+ */
+public func %~ <L: LensType> (lens: L, f: L.Part -> L.Part) -> (L.Whole -> L.Whole) {
   return lens.over(f)
 }
 
-public func <>~ <L: LensType where L.Part: Semigroup> (lens: L, a: L.Part) -> L.Whole -> L.Whole {
+/**
+ Infix operator to transform a part of a whole with a semigroup operation.
+
+ - parameter lens: A lens whose part is a semigroup.
+ - parameter a:    A part value that is concatenated to the part of a whole.
+
+ - returns: A function that transform a whole into a new whole with its part concatenated to `a`.
+ */
+public func <>~ <L: LensType where L.Part: Semigroup> (lens: L, a: L.Part) -> (L.Whole -> L.Whole) {
   return lens.over(<>a)
 }
