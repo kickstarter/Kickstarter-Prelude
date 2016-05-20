5eb9f324e3e118909ca62b8241a7075866da30aa
Brandon Williams <mbw234@gmail.com>
2016-05-19 22:06:32 -0400

    Small clean up.

--- Prelude.playground/Pages/04-Lenses.xcplaygroundpage/Contents.swift
+++ Prelude.playground/Pages/04-Lenses.xcplaygroundpage/Contents.swift
@@ -218,7 +218,7 @@ extension LensType where Whole == Project, Part == User {
 
 extension LensType where Whole == Project, Part == Location {
   var name: Lens<Project, String> {
-    return Project.lens.creator • User.lens.location • Location.lens.name
+    return Project.lens.creator.location • Location.lens.name
   }
 }
 
