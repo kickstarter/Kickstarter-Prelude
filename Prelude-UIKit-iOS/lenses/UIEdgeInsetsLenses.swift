556158fba252580f5c9bf6069bc1ae3082d6d628
Brandon Williams <mbw234@gmail.com>
2016-06-12 21:50:46 -0400

    More syntax shorts.

--- Prelude-UIKit-iOS/lenses/UIEdgeInsetsLenses.swift
+++ Prelude-UIKit-iOS/lenses/UIEdgeInsetsLenses.swift
@@ -6,22 +6,22 @@ extension UIEdgeInsets {
   public enum lens {
     public static let left = Lens<UIEdgeInsets, CGFloat>(
       view: { $0.left },
-      set: { UIEdgeInsets(top: $1.top, left: $0, bottom: $1.bottom, right: $1.right) }
+      set: { .init(top: $1.top, left: $0, bottom: $1.bottom, right: $1.right) }
     )
 
     public static let top = Lens<UIEdgeInsets, CGFloat>(
       view: { $0.left },
-      set: { UIEdgeInsets(top: $0, left: $1.left, bottom: $1.bottom, right: $1.right) }
+      set: { .init(top: $0, left: $1.left, bottom: $1.bottom, right: $1.right) }
     )
 
     public static let right = Lens<UIEdgeInsets, CGFloat>(
       view: { $0.left },
-      set: { UIEdgeInsets(top: $1.top, left: $1.left, bottom: $1.bottom, right: $0) }
+      set: { .init(top: $1.top, left: $1.left, bottom: $1.bottom, right: $0) }
     )
 
     public static let bottom = Lens<UIEdgeInsets, CGFloat>(
       view: { $0.left },
-      set: { UIEdgeInsets(top: $1.top, left: $1.left, bottom: $0, right: $1.right) }
+      set: { .init(top: $1.top, left: $1.left, bottom: $0, right: $1.right) }
     )
   }
 }
