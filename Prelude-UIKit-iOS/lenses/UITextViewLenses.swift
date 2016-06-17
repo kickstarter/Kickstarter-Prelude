58eab8f3833eee5d5e4ce7b3e341c035ffcc06d3
Stephen Celis <stephen.celis@gmail.com>
2016-06-17 17:15:26 -0400

    Add UITextView lenses (#23)
    And a few more generic ones, as well.

---
+++ Prelude-UIKit-iOS/lenses/UITextViewLenses.swift
@@ -0,0 +1,50 @@
+import Prelude
+import UIKit
+
+public protocol UITextViewProtocol: NSObjectProtocol, UITextInputTraitsProtocol {
+  var dataDetectorTypes: UIDataDetectorTypes { get set }
+  var font: UIFont? { get set }
+  var textAlignment: NSTextAlignment { get set }
+  var textColor: UIColor? { get set }
+  var textContainerInset: UIEdgeInsets { get set }
+}
+
+extension UITextView: UITextViewProtocol {}
+
+public extension LensHolder where Object: UITextViewProtocol {
+
+  public var dataDetectorTypes: Lens<Object, UIDataDetectorTypes> {
+    return Lens(
+      view: { $0.dataDetectorTypes },
+      set: { $1.dataDetectorTypes = $0; return $1 }
+    )
+  }
+
+  public var font: Lens<Object, UIFont?> {
+    return Lens(
+      view: { $0.font },
+      set: { $1.font = $0; return $1 }
+    )
+  }
+
+  public var textAlignment: Lens<Object, NSTextAlignment> {
+    return Lens(
+      view: { $0.textAlignment },
+      set: { $1.textAlignment = $0; return $1 }
+    )
+  }
+
+  public var textColor: Lens<Object, UIColor?> {
+    return Lens(
+      view: { $0.textColor },
+      set: { $1.textColor = $0; return $1 }
+    )
+  }
+
+  public var textContainerInset: Lens<Object, UIEdgeInsets> {
+    return Lens(
+      view: { $0.textContainerInset },
+      set: { $1.textContainerInset = $0; return $1 }
+    )
+  }
+}
