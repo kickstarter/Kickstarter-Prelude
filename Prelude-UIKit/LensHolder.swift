d9bc8932609a3167a3a61f62f63785584d456a79
Brandon Williams <mbw234@gmail.com>
2016-07-05 09:51:15 -0400

    Added a reflect lens.

--- Prelude-UIKit/LensHolder.swift
+++ Prelude-UIKit/LensHolder.swift
@@ -1,4 +1,5 @@
 import Foundation
+import Prelude
 
 public protocol LensObject: class {}
 
@@ -9,5 +10,11 @@ public struct LensHolder <Object: LensObject> {}
 public extension LensObject {
   public static var lens: LensHolder<Self> {
     return LensHolder()
+  }
+}
+
+public extension LensHolder where Object: NSObjectProtocol {
+  public var reflect: Lens<Object, Object> {
+    return Lens(view: { $0 }, set: { part, _ in part })
   }
 }
