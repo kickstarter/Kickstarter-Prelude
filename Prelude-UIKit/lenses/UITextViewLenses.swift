97b3ef05dfdc86f58f8a420d0dd32ecff095b359
Brandon Williams <mbw234@gmail.com>
2016-07-05 14:54:54 -0400

    Added text view lenses to tvos target and fenced iOS specific APIs.

--- Prelude-UIKit/lenses/UITextViewLenses.swift
+++ Prelude-UIKit/lenses/UITextViewLenses.swift
@@ -2,7 +2,9 @@ import Prelude
 import UIKit
 
 public protocol UITextViewProtocol: UIViewProtocol, UITextInputTraitsProtocol {
+  #if os(iOS)
   var dataDetectorTypes: UIDataDetectorTypes { get set }
+  #endif
   var font: UIFont? { get set }
   var textAlignment: NSTextAlignment { get set }
   var textColor: UIColor? { get set }
@@ -13,12 +15,14 @@ extension UITextView: UITextViewProtocol {}
 
 public extension LensHolder where Object: UITextViewProtocol {
 
+  #if os(iOS)
   public var dataDetectorTypes: Lens<Object, UIDataDetectorTypes> {
     return Lens(
       view: { $0.dataDetectorTypes },
       set: { $1.dataDetectorTypes = $0; return $1 }
     )
   }
+  #endif
 
   public var font: Lens<Object, UIFont?> {
     return Lens(
