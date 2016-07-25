2a10f1fa30b49f74bc6656050e15e98c811a2015
Christopher Wright <christopherwright@gmail.com>
2016-07-25 10:26:18 -0400

    Remove unnecessary Orderings

--- PreludeTests/OrderingTests.swift
+++ PreludeTests/OrderingTests.swift
@@ -3,15 +3,15 @@ import XCTest
 
 final class OrderingTests: XCTestCase {
   func testOp() {
-    XCTAssertEqual(Ordering.lt, Ordering.lt.op(Ordering.lt))
-    XCTAssertEqual(Ordering.lt, Ordering.lt.op(Ordering.eq))
-    XCTAssertEqual(Ordering.lt, Ordering.lt.op(Ordering.gt))
-    XCTAssertEqual(Ordering.lt, Ordering.eq.op(Ordering.lt))
-    XCTAssertEqual(Ordering.eq, Ordering.eq.op(Ordering.eq))
-    XCTAssertEqual(Ordering.gt, Ordering.eq.op(Ordering.gt))
-    XCTAssertEqual(Ordering.gt, Ordering.gt.op(Ordering.lt))
-    XCTAssertEqual(Ordering.gt, Ordering.gt.op(Ordering.eq))
-    XCTAssertEqual(Ordering.gt, Ordering.gt.op(Ordering.gt))
+    XCTAssertEqual(Ordering.lt, Ordering.lt.op(.lt))
+    XCTAssertEqual(Ordering.lt, Ordering.lt.op(.eq))
+    XCTAssertEqual(Ordering.lt, Ordering.lt.op(.gt))
+    XCTAssertEqual(Ordering.lt, Ordering.eq.op(.lt))
+    XCTAssertEqual(Ordering.eq, Ordering.eq.op(.eq))
+    XCTAssertEqual(Ordering.gt, Ordering.eq.op(.gt))
+    XCTAssertEqual(Ordering.gt, Ordering.gt.op(.lt))
+    XCTAssertEqual(Ordering.gt, Ordering.gt.op(.eq))
+    XCTAssertEqual(Ordering.gt, Ordering.gt.op(.gt))
   }
 
   func testReversed() {
