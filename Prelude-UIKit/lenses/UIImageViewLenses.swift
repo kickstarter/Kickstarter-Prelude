988ecb56968f087585df7731dfcda53cdf30b279
Brandon Williams <mbw234@gmail.com>
2016-08-06 13:46:32 -0400

    Added an image lens to image views.

--- Prelude-UIKit/lenses/UIImageViewLenses.swift
+++ Prelude-UIKit/lenses/UIImageViewLenses.swift
@@ -1,6 +1,17 @@
 import Prelude
 import UIKit
 
-public protocol UIImageViewProtocol: UIViewProtocol {}
+public protocol UIImageViewProtocol: UIViewProtocol {
+  var image: UIImage? { get set }
+}
 
 extension UIImageView: UIImageViewProtocol {}
+
+extension LensHolder where Object: UIImageViewProtocol {
+  public var image: Lens<Object, UIImage?> {
+    return Lens(
+      view: { $0.image },
+      set: { $1.image = $0; return $1 }
+    )
+  }
+}
