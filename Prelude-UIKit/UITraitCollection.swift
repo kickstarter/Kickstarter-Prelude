bd8574f437d06138378f0bfb8239aec4b7bd9524
Brandon Williams <mbw234@gmail.com>
2016-08-11 11:17:35 -0400

    Docs.

--- Prelude-UIKit/UITraitCollection.swift
+++ Prelude-UIKit/UITraitCollection.swift
@@ -1,10 +1,12 @@
 import UIKit
 
 extension UITraitCollection {
+  /// Returns true if the trait collection is both horizontally and vertically regular.
   public var isRegularRegular: Bool {
     return self.horizontalSizeClass == .Regular && self.verticalSizeClass == .Regular
   }
 
+  /// Returns true if the trait collection is vertically compact.
   public var isVerticallyCompact: Bool {
     return self.verticalSizeClass == .Compact
   }
