672b8017f0188f6ee0a86df571b107bbee13abbb
Brandon Williams <mbw234@gmail.com>
2016-08-23 11:04:47 -0400

    A few more lenses. (#47)

---
+++ Prelude-UIKit/lenses/UIActivityIndicatorViewLenses.swift
@@ -0,0 +1,34 @@
+import Prelude
+import UIKit
+
+public protocol UIActivityIndicatorViewProtocol: UIViewProtocol {
+  var activityIndicatorViewStyle: UIActivityIndicatorViewStyle { get set }
+  var color: UIColor? { get set }
+  var hidesWhenStopped: Bool { get set }
+}
+
+extension UIActivityIndicatorView: UIActivityIndicatorViewProtocol {}
+
+public extension LensHolder where Object: UIActivityIndicatorViewProtocol {
+
+  public var activityIndicatorViewStyle: Lens<Object, UIActivityIndicatorViewStyle> {
+    return Lens(
+      view: { $0.activityIndicatorViewStyle },
+      set: { $1.activityIndicatorViewStyle = $0; return $1 }
+    )
+  }
+
+  public var color: Lens<Object, UIColor?> {
+    return Lens(
+      view: { $0.color },
+      set: { $1.color = $0; return $1 }
+    )
+  }
+
+  public var hidesWhenStopped: Lens<Object, Bool> {
+    return Lens(
+      view: { $0.hidesWhenStopped },
+      set: { $1.hidesWhenStopped = $0; return $1 }
+    )
+  }
+}
---
+++ Prelude-UIKit/lenses/UIWebViewLenses.swift
@@ -0,0 +1,36 @@
+import Prelude
+import WebKit
+
+public protocol UIWebViewProtocol: UIViewProtocol {
+  var scrollView: UIScrollView { get }
+  var suppressesIncrementalRendering: Bool { get set }
+}
+
+extension UIWebView: UIWebViewProtocol {}
+
+public extension LensHolder where Object: UIWebViewProtocol {
+
+  public var scrollView: Lens<Object, UIScrollView> {
+    return Lens(
+      view: { $0.scrollView },
+      set: { $1 }
+    )
+  }
+
+  public var suppressesIncrementalRendering: Lens<Object, Bool> {
+    return Lens(
+      view: { $0.suppressesIncrementalRendering },
+      set: { $1.suppressesIncrementalRendering = $0; return $1 }
+    )
+  }
+}
+
+extension LensType where Whole: UIWebViewProtocol, Part == UIScrollView {
+  public var delaysContentTouches: Lens<Whole, Bool> {
+    return Whole.lens.scrollView • Part.lens.delaysContentTouches
+  }
+
+  public var decelerationRate: Lens<Whole, CGFloat> {
+    return Whole.lens.scrollView • Part.lens.decelerationRate
+  }
+}
