e416c10c06badd911efeeb79d620dd21fd7ddcbd
Brandon Williams <mbw234@gmail.com>
2017-03-30 15:04:28 -0400

    fix tuple equality and opt out of swiftlint rule

--- Prelude/Tuple.swift
+++ Prelude/Tuple.swift
@@ -1,5 +1,7 @@
 public func == <A: Equatable, B: Equatable> (lhs: [(A, B)], rhs: [(A, B)]) -> Bool {
-  for (idx, _) in lhs.enumerated() {
+  guard lhs.count == rhs.count else { return false }
+
+  for idx in lhs.indices {
     if lhs[idx] != rhs[idx] {
       return false
     }
