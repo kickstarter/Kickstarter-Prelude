088334911860349febc9b98a852d88293b4a6a93
Brandon Williams <mbw234@gmail.com>
2017-03-30 16:39:26 -0400

    Xcode 8.3 support (#70)
    * update to xcode 8.3
    
    * test
    
    * fix tuple equality and opt out of swiftlint rule

--- Prelude/TupleTests.swift
+++ Prelude/TupleTests.swift
@@ -2,11 +2,14 @@ import XCTest
 @testable import Prelude
 
 final class TupleTests: XCTestCase {
+
   func testArrayOfTuplesEquality() {
     XCTAssert([(1, 2), (3, 4)] == [(1, 2), (3, 4)])
 
     XCTAssertFalse([(1, 2), (3, 4)] == [(1, 0), (3, 4)])
     XCTAssertFalse([(1, 2), (3, 4)] == [(1, 2), (0, 4)])
+    XCTAssertFalse([(1, 2), (3, 4)] == [(1, 2), (0, 4), (0, 0)])
+    XCTAssertFalse([(1, 2), (3, 4), (0, 0)] == [(1, 2), (0, 4)])
   }
 
   func testFirst() {
