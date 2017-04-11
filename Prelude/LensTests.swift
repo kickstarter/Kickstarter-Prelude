273bba7fb9b56f6fa4e35da054e4b4faa91eab09
Brandon Williams <mbw234@gmail.com>
2017-04-11 19:28:35 -0400

    Updated some deprecated operators.

--- Prelude/LensTests.swift
+++ Prelude/LensTests.swift
@@ -14,8 +14,8 @@ final class LensTests: XCTestCase {
   }
 
   func testLensCompositionOperator() {
-    XCTAssertEqual(2, (User._location • Location._id).view(user))
-    XCTAssertEqual(3, (User._location • Location._city • City._id).view(user))
+    XCTAssertEqual(2, (User._location..Location._id).view(user))
+    XCTAssertEqual(3, (User._location..Location._city..City._id).view(user))
   }
 
   func testLensSetOperator() {
@@ -23,7 +23,7 @@ final class LensTests: XCTestCase {
   }
 
   func testLensSetAndCompositionOperatorPrecedence() {
-    XCTAssertEqual(4, ((User._location • Location._id) .~ 4)(user).location.id)
+    XCTAssertEqual(4, ((User._location..Location._id) .~ 4)(user).location.id)
   }
 
   func testLensViewOperator() {
@@ -31,7 +31,7 @@ final class LensTests: XCTestCase {
   }
 
   func testLensViewAndCompositionOperatorPrecedence() {
-    XCTAssertEqual(2, user ^* User._location • Location._id)
+    XCTAssertEqual(2, user ^* User._location..Location._id)
   }
 
   func testComporator() {
@@ -52,26 +52,26 @@ final class LensTests: XCTestCase {
   }
 
   func testOverAndCompositionOperatorPrecedence() {
-    XCTAssertEqual(3, ((User._location • Location._id) %~ incr)(user).location.id)
+    XCTAssertEqual(3, ((User._location..Location._id) %~ incr)(user).location.id)
   }
 
   func testOperatorPrecedences() {
     XCTAssertEqual(User(id: 11, location: Location(id: 12, city: City(id: 13)), name: "brando"),
       user
         |> User._id %~ add(10)
-        |> (User._location • Location._id) %~ square
-        |> (User._location • Location._id) %~ add(8)
-        |> (User._location • Location._city • City._id) .~ 13
+        |> (User._location..Location._id) %~ square
+        |> (User._location..Location._id) %~ add(8)
+        |> (User._location..Location._city..City._id) .~ 13
         |> User._name .~ "brando"
     )
 
     XCTAssertEqual(13,
       user
         |> User._id %~ add(10)
-        |> (User._location • Location._id) %~ square
-        |> (User._location • Location._id) %~ add(8)
-        |> (User._location • Location._city • City._id) .~ 13
-        ^* (User._location • Location._city • City._id)
+        |> (User._location..Location._id) %~ square
+        |> (User._location..Location._id) %~ add(8)
+        |> (User._location..Location._city..City._id) .~ 13
+        ^* (User._location..Location._city..City._id)
     )
   }
 
