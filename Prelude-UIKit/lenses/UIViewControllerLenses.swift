ddfce4abdb17807880f1251d1bee8dc17f484cf5
Stephen Celis <stephen@stephencelis.com>
2017-08-24 11:49:14 -0400

    Remove redundant NSObjectProtocol conformance
    Silences a new warning.

--- Prelude-UIKit/lenses/UIViewControllerLenses.swift
+++ Prelude-UIKit/lenses/UIViewControllerLenses.swift
@@ -1,7 +1,7 @@
 import Prelude
 import UIKit
 
-public protocol UIViewControllerProtocol: NSObjectProtocol, UITraitEnvironmentProtocol, LensObject {
+public protocol UIViewControllerProtocol: UITraitEnvironmentProtocol, LensObject {
   var navigationController: UINavigationController? { get }
   var navigationItem: UINavigationItem { get }
   var title: String? { get set }
