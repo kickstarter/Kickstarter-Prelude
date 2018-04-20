2a03c968273f989eefdeb721edfde6512cca566f
Saturnino Collaço Teixeira Filho <scollaco@gmail.com>
2018-04-20 12:12:57 -0400

    Added UISwitch lenses (#84)

---
+++ Prelude-UIKit/lenses/UISwitchLenses.swift
@@ -0,0 +1,42 @@
+import Prelude
+import UIKit
+
+public protocol UISwitchProtocol: UIControlProtocol {
+  var isOn: Bool { get set }
+  var onTintColor: UIColor? { get set }
+  var thumbTintColor: UIColor? { get set }
+  var tintColor: UIColor! { get set }
+}
+
+extension UISwitch: UISwitchProtocol {}
+
+public extension LensHolder where Object: UISwitchProtocol {
+
+  public var isOn: Lens<Object, Bool> {
+    return Lens(
+      view: { $0.isOn },
+      set: { $1.isOn = $0; return $1; }
+    )
+  }
+
+  public var onTintColor: Lens<Object, UIColor?> {
+    return Lens(
+      view: { $0.onTintColor },
+      set: { $1.onTintColor = $0; return $1; }
+    )
+  }
+
+  public var thumbTintColor: Lens<Object, UIColor?> {
+    return Lens(
+      view: { $0.thumbTintColor },
+      set: { $1.thumbTintColor = $0; return $1; }
+    )
+  }
+
+  public var tintColor: Lens<Object, UIColor> {
+    return Lens(
+      view: { $0.tintColor },
+      set: { $1.tintColor = $0; return $1; }
+    )
+  }
+}
