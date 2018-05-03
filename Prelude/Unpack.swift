033bc8e50437e011ac178953e696e8e4134cacf5
Isabel Barrera <ifbarrera@me.com>
2018-05-03 15:41:56 -0400

    Extending unpack

--- Prelude/Unpack.swift
+++ Prelude/Unpack.swift
@@ -21,3 +21,15 @@ public func unpack <A, B, C> (_ ab: (A, B), c: C) -> (A, B, C) {
 public func unpack <A, B, C> (_ a: A, bc: (B, C)) -> (A, B, C) {
   return (a, bc.0, bc.1)
 }
+
+/**
+ Unpacks two tuples into a flat tuple.
+ 
+ - parameter ab: A tuple
+ - parameter cd: A tuple
+ 
+ - returns: A flattened 4-tuple.
+ */
+public func unpack <A, B, C, D> (_ ab: (A, B), _ cd: (C, D)) -> (A, B, C, D) {
+  return (ab.0, ab.1, cd.0, cd.1)
+}
---
+++ Prelude/UnpackTests.swift
@@ -0,0 +1,37 @@
+//
+//  UnpackTests.swift
+//  Prelude-iOSTests
+//
+//  Created by Isabel Barrera on 5/3/18.
+//  Copyright © 2018 Kickstarter. All rights reserved.
+//
+
+import XCTest
+@testable import Prelude
+
+class UnpackTests: XCTestCase {
+  func testABUnpack() {
+    let testTuple = unpack(("a", "b"), c: "c")
+    
+    XCTAssertEqual(testTuple.0, "a")
+    XCTAssertEqual(testTuple.1, "b")
+    XCTAssertEqual(testTuple.2, "c")
+  }
+  
+  func testBCUnpack() {
+    let testTuple = unpack("a", bc: ("b", "c"))
+    
+    XCTAssertEqual(testTuple.0, "a")
+    XCTAssertEqual(testTuple.1, "b")
+    XCTAssertEqual(testTuple.2, "c")
+  }
+  
+  func testABCDUnpack() {
+    let testTuple = unpack(("a", "b"), ("c", "d"))
+
+    XCTAssertEqual(testTuple.0, "a")
+    XCTAssertEqual(testTuple.1, "b")
+    XCTAssertEqual(testTuple.2, "c")
+    XCTAssertEqual(testTuple.3, "d")
+  }
+}
