54b8b77b50da664675d7d6d32f611ce806af44cc
Isabel Barrera <ifbarrera@me.com>
2018-05-03 16:03:55 -0400

    Swiftlint

--- Prelude/UnpackTests.swift
+++ Prelude/UnpackTests.swift
@@ -4,20 +4,20 @@ import XCTest
 class UnpackTests: XCTestCase {
   func testABUnpack() {
     let testTuple = unpack(("a", "b"), c: "c")
-    
+
     XCTAssertEqual(testTuple.0, "a")
     XCTAssertEqual(testTuple.1, "b")
     XCTAssertEqual(testTuple.2, "c")
   }
-  
+
   func testBCUnpack() {
     let testTuple = unpack("a", bc: ("b", "c"))
-    
+
     XCTAssertEqual(testTuple.0, "a")
     XCTAssertEqual(testTuple.1, "b")
     XCTAssertEqual(testTuple.2, "c")
   }
-  
+
   func testABCDUnpack() {
     let testTuple = unpack(("a", "b"), ("c", "d"))
 
