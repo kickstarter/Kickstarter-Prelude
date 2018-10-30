1db35c131be7797c5129937c304e2787329dc8e0
Christella Dolmo <cdolm92@gmail.com>
2018-10-30 12:18:31 -0400

    haptic feedback extension to uibutton

--- Prelude-UIKit/UIButton.swift
+++ Prelude-UIKit/UIButton.swift
@@ -10,4 +10,16 @@ extension UIButton {
   public func setBackgroundColor(_ backgroundColor: UIColor, for state: UIControlState) {
     self.setBackgroundImage(.pixel(ofColor: backgroundColor), for: state)
   }
+
+  @available(iOSApplicationExtension 10.0, *)
+  public func generateSelectionFeedback() {
+    let notification = UISelectionFeedbackGenerator()
+    notification.selectionChanged()
+  }
+
+  @available(iOSApplicationExtension 10.0, *)
+  public func generateSuccessFeedback() {
+    let notification = UINotificationFeedbackGenerator()
+    notification.notificationOccurred(.success)
+  }
 }
